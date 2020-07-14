4e668c4c55f342631803dab9f54216e9fa8d3514
David Tong <ytong@ebay.com>
2020-07-14 04:13:42 -0500

    Working Version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 Vue.config.productionTip = false
+
+Vue.use(CKEditor);
 
 new Vue({
   render: h => h(App),
